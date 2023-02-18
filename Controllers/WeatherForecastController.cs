2b73c25658575ff8821aa9205420a10954895c6b
geokal <gkalpak@fogus.gr>
2023-02-18 14:04:57 +0200

    initial commit

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Mvc;
+
+using System.Text.Json;
+
+namespace Serilog.Test.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    private readonly ILogger<WeatherForecastController> logger;
+
+    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    {
+        this.logger = logger;
+    }
+
+    [HttpGet(Name = "GetWeatherForecast")]
+    public IEnumerable<WeatherForecast> Get()
+    {
+        logger.LogDebug("Inside GetWeatherForecast endpoint");
+        var response = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        {
+            Date = DateTime.Now.AddDays(index),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        })
+        .ToArray();
+        logger.LogDebug($"The response for the get weather forecast is {JsonSerializer.Serialize(response)}");
+        return response;
+    }
+}
